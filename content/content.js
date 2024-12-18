3a5261f74a9eafb0755959a45eda49a8771ca716
islom2202 <islomihsan@gmail.com>
2024-12-18 16:06:11 +0500

    added jivo navigation

--- content/content.js
+++ content/content.js
@@ -45,11 +45,34 @@ async function copyPaste(text) {
 }
 
 
-// fill tags for app activation
+// alt hotkey functions
 document.onkeydown = async (e) => {
-  if (e.altKey && e.key === "s") {
   e.preventDefault()
-  try {
+  // tags filling functions
+  if (e.altKey && e.key === "s") {
+    appActivation()
+  }
+  if (e.altKey && e.key === "o") {
+    onlineCredit()
+  }
+  // navigation inside jivo
+  if(e.altKey && e.key === "ArrowLeft") {
+    document.querySelector('[aria-label="Входящие"]').click();
+  }
+  if(e.altKey && e.key === "ArrowUp"){
+    document.querySelector('[aria-label="Мои"]').click();
+  }
+  if (e.altKey && e.key === "ArrowRight") {
+    document.querySelector('[aria-label="Все"]').click()
+  }
+  if (e.altKey && e.key === "ArrowDown") {
+    document.querySelector('[class="listItem"]').click()
+  }
+}
+
+
+async function appActivation(){
+    try {
       // initialize function
       async function performAction(element, action) {
         element.click()
@@ -88,10 +111,47 @@ document.onkeydown = async (e) => {
         '[title="Приложение не работает"]'
       )
       await performAction(subtopic, "click")
+    } catch (error) {
+      console.error("Something went wrong")
     }
-  catch (error) {
-    console.error('Something went wrong');
-  }
-  }
 }
-// fill tags for online request for credit
+
+async function onlineCredit() {
+    // Select Akram
+    const authors = document.querySelector(
+      '[data-qa-id="select-agent-selected-name"]'
+    )
+    await authors.click()
+    const akram = document.querySelector('[data-qa-id="select-agent-item-2"]')
+    await akram.click()
+    // Choose a tag
+    const tags = document.querySelector('[data-qa-id="tags-control-input"]')
+    await tags.click()
+    new Promise((resolve) => setTimeout(resolve, 250))
+    document.querySelector('[title="Банкомат"]').scrollIntoView({ block: "start" })
+
+    // to wait after the scroll for 500 milliseconds before running following code
+    new Promise((resolve) => setTimeout(resolve, 250))
+    const onlineCreditTag = document.querySelector('[title="онлайн карз"]')
+    await onlineCreditTag.click()
+    // Fill the field with description
+    const descriptionField = document.querySelector(
+      '[data-qa-id="ipanel-description-textarea"]'
+    )
+    await descriptionField.click();
+    const descriptionText = "Заявка на онлайн кредит - Экспресс"
+    descriptionField.value = descriptionText
+    // Fill topic
+    const topicContainer = document.querySelector(
+      '[data-qa-id="topic-control-container"]'
+    )
+    await topicContainer.click();
+    document.querySelector('[title="Режим работы"]').scrollIntoView({block: "start"})
+    new Promise((resolve) => setTimeout(resolve, 250))
+    const topic = document.querySelector('[title="Кредит"]')
+    await topic.click();
+    const subtopic = document.querySelector('[title="Онлайн кредит"]')
+    await subtopic.click();
+}
+
+
